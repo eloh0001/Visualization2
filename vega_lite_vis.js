2f4bfc85d5305370784328c9184e4e42b301388c
eloh0001 <89181390+eloh0001@users.noreply.github.com>
2021-10-10 03:53:58 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "beer_barchart.vg.json";
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+
+}).catch(console.error);
---
+++ vega_lite_vis1.js
@@ -0,0 +1,5 @@
+var vg_2 = "mapchart.vg.json";
+
+vegaEmbed("#map_chart", vg_2).then(function(result) {
+
+}).catch(console.error);
